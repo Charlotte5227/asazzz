cfad6a1fee54df52eb768ac7d46ea39a263d09fe
Charlotte5227 <h.kanato.1019@gmail.com>
2026-02-20 17:04:34 +0900

    Update MainViewModel.cs

--- MauiGithubActionsSample/MainViewModel.cs
+++ MauiGithubActionsSample/MainViewModel.cs
@@ -2,7 +2,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace WarRandomApp;
+namespace MauiGithubActionsSample;
 
 public sealed class MainViewModel : INotifyPropertyChanged
 {
